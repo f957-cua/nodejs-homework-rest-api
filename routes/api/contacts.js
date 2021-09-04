44c9f3ba8fff758db6f7423dd752f97a9697768a
unknown <ivanfiliptschik00@gmail.com>
2021-09-04 15:53:33 +0300

    add controllers

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,85 +1,19 @@
 const express = require('express')
 const router = express.Router()
-const {Contact} = require('../../models')
+
+const ctrl = require('../../controllers/contacts')
 const { checkCreateContact, checkUpdateContact } = require('./validation')
 
-router.get('/', async (req, res, next) => {
-  try {
-    const data = await Contact.find({})
-    res.json({ status: 'success', code: 200, data: data })
-  } catch (error) {
-    next(error)
-  }
-})
+router.get('/', ctrl.getAll)
 
-router.get('/:contactId', async (req, res, next) => {
-  try {
-    const item = await Contact.findById(req.params.contactId)
-    if (item) {
-        res.json({ status: 'success', code: 200, data: item })
-        return
-    }
-    res.json({ status: 'error', code: 404, message: 'Not found' })
-  } catch (error) {
-    next(error)
-  }
-})
+router.get('/:contactId', ctrl.getById)
 
-router.post('/', checkCreateContact, async (req, res, next) => {
-  try {
-    const item = await Contact.create(req.body)
-    if (item) {
-        res.json({ status: 'success', code: 201, data: {favorite: false, ...item} })
-        return
-    }
-    res.json({ status: 'error', code: 404, message: 'Not found' })
-  } catch (error) {
-    next(error)
-  }
-})
+router.post('/', checkCreateContact, ctrl.post)
 
-router.delete('/:contactId', async (req, res, next) => {
-  try {
-    const contact = await Contact.findByIdAndDelete(req.params.contactId)
-    if (contact) {
-        res.json({ status: 'success', code: 200, message: 'contact deleted', data: contact })
-        return
-    }
-    res.json({ status: 'error', code: 404, message: 'Not found' })
-  } catch (error) {
-    next(error)
-  }
-})
+router.delete('/:contactId', ctrl.remove)
 
-router.put('/:contactId', checkUpdateContact, async (req, res, next) => {
-  try {
-    const contact = await Contact.findByIdAndUpdate(req.params.contactId, req.body, {new: true})
-    if (contact) {
-        res.json({ status: 'success', code: 200, data: { contact }})
-        return
-    }
-    res.json({ status: 'error', code: 404, message: 'Not found' })
-  } catch (error) {
-    next(error)
-  }
-})
+router.put('/:contactId', checkUpdateContact, ctrl.put)
 
-router.patch('/:contactId/favorite', async (req, res, next) => {
-  try {
-    const {favorite} = req.body
-    if (!favorite) {
-      res.json({ status: 'error', code: 400, message: 'missing field favorite' })
-      return
-    }
-    const contact = await Contact.findByIdAndUpdate(req.params.contactId, {favorite}, {new: true})
-    if (contact) {
-        res.json({ status: 'success', code: 200, data: { contact }})
-        return
-    }
-    res.json({ status: 'error', code: 404, message: 'Not found' })
-  } catch (error) {
-    next(error)
-  }
-})
+router.patch('/:contactId/favorite', ctrl.patch)
 
 module.exports = router
