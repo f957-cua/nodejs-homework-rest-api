6d9160e9f0b1ef9b79789be6d1581dbba395beea
unknown <ivanfiliptschik00@gmail.com>
2021-09-13 20:53:32 +0300

    step_1

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(helmet())
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json({ limit: 10000 }))
+app.use(express.static('public'))
 
 app.use('/api/', rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
